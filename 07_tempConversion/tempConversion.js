e378343e15ac7c7e5c6427edb85e280467c4ec38
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-06-25 03:44:33 -0400

    ain't no way it was wrong cause of the parenthesis extra at the ends

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fair) {
+  return Math.round(((fair - 32) * 5/9) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cells) {
+  return Math.round(((cells * 9/5) + 32) * 10) / 10
 };
 
 // Do not edit below this line
