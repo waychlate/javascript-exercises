f99b63a7ea529795017a63197d2901ff0610a531
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-06-25 03:21:47 -0400

    I am so bad :cryingface:

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if ((numOne < 0 || numTwo < 0) || (typeof numOne != 'number' || typeof numTwo != 'number')) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let firstNum = (numOne < numTwo) ? numOne : numTwo;
+    let secondNum = (firstNum == numOne) ? numTwo : numOne;
+
+    for (; firstNum <= secondNum; firstNum++) {
+        sum += firstNum;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
