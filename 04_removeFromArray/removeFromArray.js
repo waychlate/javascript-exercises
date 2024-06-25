991ba87c17fd4afee75d4bdb84a1ea2c60562286
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-06-24 22:43:49 -0400

    really bad and inefficient solution, but I'm glad I was able to think of something

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let numArray = args[0]
+    let newArray = []
+
+    for (v = 0; v < numArray.length; v++) {
+
+        for (i = 1; i < args.length; i++) {
+            if (numArray[v] === args[i]) {
+                delete numArray[v]
+            }
+            
+        }
+
+    }
+    for (const arr of numArray) {
+        if (arr != undefined) {
+            newArray.push(arr)
+        }
+    }
+
+    return newArray
+    
 };
 
 // Do not edit below this line
