005ee3b5c0bcfe76264582c0d9320589cf213924
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-06-24 20:57:04 -0400

    Finished exercise three in record time

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let returnedString = '';
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        let char = string[i]
+        returnedString += char;
+    }
+    return returnedString;
 };
 
 // Do not edit below this line
