2861bd6f507bd551a9192c6739466bbbc7087656
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-06-30 02:49:05 -0400

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let sorted = array.sort((a, b) => {
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        if (isNaN(aAge)) aAge = 2024 - a.yearOfBirth;
+        if (isNaN(bAge)) bAge = 2024 - b.yearOfBirth;
+        console.log(aAge + ' ' + bAge)
 
+        return (bAge) - (aAge);
+    })
+    
+    return sorted[0];
 };
 
 // Do not edit below this line
