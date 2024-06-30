0f038740ea481d3770ade7a7a6836e6fedb1894f
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-06-29 21:54:27 -0400

    messy and I was confused for this one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS"
+    if (num == 0) return 0;
+    let a = 0;
+    let b = 1;
+    let c = 1;
+    for (let i = 0; i < Number(num) - 1; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
